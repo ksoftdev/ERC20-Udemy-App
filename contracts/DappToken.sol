1bbf7eab0063124c64299965c384627667390d80
ksoftdev <kuntursoftdev@gmail.com>
2021-02-26 14:13:08 -0500

    1. Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.2;
+
+contract DappToken {
+  // Constructor
+  uint256 public totalSupply;
+
+  constructor () public {
+    totalSupply = 1000000;
+  }
+
+  // Set the total number of tokens
+
+  // Read the total number of tokens
+}
