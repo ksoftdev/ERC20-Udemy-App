99d40ba0429d9c22d8606e77e943e3bc24e30b97
ksoftdev <kuntursoftdev@gmail.com>
2021-02-26 20:00:26 -0500

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,13 +2,42 @@ pragma solidity >=0.4.2;
 
 contract DappToken {
   // Constructor
+  // Name
+  string public name = 'Dapp Token';
+  // Symbol
+  string public symbol = 'DAPP';
+  // standard
+  string public standard = 'DApp Token v1.0';
+  // Supply
   uint256 public totalSupply;
 
-  constructor () public {
-    totalSupply = 1000000;
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
+
+  mapping(address => uint256) public balanceOf;
+
+  constructor (uint256 _initialSupply) public {
+    // msg is a global variable from solidity.
+    // sender is the address of the account that deployed the contract.
+    balanceOf[msg.sender] = _initialSupply;
+
+    // Allocate the initial supply.
+    totalSupply = _initialSupply;
   }
 
-  // Set the total number of tokens
+  // Transfering funds.
+  function transfer(address _to, uint256 _value) public returns (bool success){
+    require(balanceOf[msg.sender] >= _value);
+    // Transfer the balance
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
 
-  // Read the total number of tokens
+    // Transfer event
+    emit Transfer(msg.sender, _to, _value);
+
+    return true;
+  }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 const DappToken = artifacts.require("DappToken");
 
+// Set the total number of tokens
 module.exports = function (deployer) {
-  deployer.deploy(DappToken);
+  deployer.deploy(DappToken, 1000000);
 };
